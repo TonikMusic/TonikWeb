1f9bb981f969afe7a915cded6cea5c600cb0e3c3
KhallilB <khallilbailey@gmail.com>
2019-09-11 10:09:42 -0700

    [add] Jest configurations

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Nav.css';
 
-function Nav() {
-  return <div> Navbar works! </div>;
-}
+class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      isArtist: false,
+    };
+  }
 
+  render() {
+    return (
+      <div className="nav_container">
+        <nav>
+          <div className="left_menu" />
+        </nav>
+      </div>
+    );
+  }
+}
 export default Nav;
