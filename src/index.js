b3e17f4d5ecf02a4f38419663f27adc628918266
KhallilB <khallilbailey@gmail.com>
2019-09-11 11:29:08 -0700

    [add] fix merge confilct

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import App from './App';
-import Nav from './components/navigation/Nav';
-
-const appRoutes = (
-  <Router>
-    <Nav />
-    <div>
-      <Route exact path="/" component={App} />
-    </div>
-  </Router>
-);
-
-ReactDOM.render(appRoutes, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
