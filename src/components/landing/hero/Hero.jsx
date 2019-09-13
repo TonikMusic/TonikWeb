15ff47be83b8423b2c46bc0a316584b0a75566a0
KhallilB <khallilbailey@gmail.com>
2019-09-13 16:25:29 -0700

    [add] finish hero

--- src/components/landing/hero/Hero.jsx
+++ src/components/landing/hero/Hero.jsx
@@ -9,7 +9,9 @@ function Hero() {
         <h1>
           <span id="hero__heading_primary">Build Your Brand</span>
         </h1>
-        <h3>Find opprotunity with Tonik</h3>
+        <h3>
+          <span id="hero__heading_secondary">Find opprotunity with Tonik create and expand your community</span>
+        </h3>
       </div>
     </div>
   );
