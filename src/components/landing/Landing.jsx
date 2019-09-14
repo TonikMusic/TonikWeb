35f23c69da23c34bb462493cdb3dbba8229bd6a7
KhallilB <khallilbailey@gmail.com>
2019-09-13 18:40:09 -0700

    [add] About component

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './Landing.css';
 
 import Hero from './hero/Hero';
+import About from './about/About';
 
 function Landing() {
   return (
     <div>
       <Hero />
+      <About />
     </div>
   );
 }
---
+++ src/components/landing/about/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return <div>About Working</div>;
+}
+
+export default About;
