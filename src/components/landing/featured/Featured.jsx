1e0283e85c77d374cb428de0f04d46e453c994fc
KhallilB <khallilbailey@gmail.com>
2019-09-13 23:00:19 -0700

    [add] Rendering Featured artists, imgs, genres

--- src/components/landing/featured/Featured.jsx
+++ src/components/landing/featured/Featured.jsx
@@ -6,20 +6,21 @@ const Featured = () => (
   <div id="featured" className="landing_featured">
     <h3 className="landing_featured__title">Featured Artists</h3>
     <div className="landing_featured__artists">
-      {featuredArtistList.map((artist) => (
-        // <div>{artist.name}</div>
-        <div className="landing_featured__row">
-          <div className="landing_featured__column">
+      <div className="landing_featured__row">
+        <div className="landing_featured__column">
+          {featuredArtistList.map((artist) => (
             <div className="landing_featured__card">
               <img src={artist.profile_img} alt="#" />
               <h2>{artist.name}</h2>
               {artist.genre.map((genres) => (
-                <h4>{genres}</h4>
+                <button type="button" className="landing_featured__artists_genres">
+                  {genres}
+                </button>
               ))}
             </div>
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   </div>
 );
