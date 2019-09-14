c91b1384bd6518ac89ca23d3d1564a8e05570442
KhallilB <khallilbailey@gmail.com>
2019-09-13 20:06:51 -0700

    [add] Featured Component

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -3,12 +3,14 @@ import './Landing.css';
 
 import Hero from './hero/Hero';
 import About from './about/About';
+import Featured from './featured/Featured';
 
 function Landing() {
   return (
     <div>
       <Hero />
       <About />
+      <Featured />
     </div>
   );
 }
---
+++ src/components/landing/featured/Featured.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Featured.css';
+
+function Featured() {
+  return <div>Featured Working!</div>;
+}
+
+export default Featured;
