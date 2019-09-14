4b75e8f5ef3e42ab8edfdd33cae8a5d382bee2cf
KhallilB <khallilbailey@gmail.com>
2019-09-13 22:25:10 -0700

    [add] mapped out artist data

--- src/components/landing/featured/Featured.jsx
+++ src/components/landing/featured/Featured.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Featured.css';
+import featuredArtistList from '../../../data/dummy/featuredArtistList.json';
 
 const Featured = () => (
   <div id="featured" className="landing_featured">
     <h3 className="landing_featured__title">Featured Artists</h3>
+    <div className="landing_featured__artists">
+      {featuredArtistList.map((artist) => (
+        // <div>{artist.name}</div>
+        <div className="landing_featured__row">
+          <div className="landing_featured__column">
+            <div className="landing_featured__card">
+              <img src={artist.profile_img} alt="#" />
+              <h2>{artist.name}</h2>
+              {artist.genre.map((genres) => (
+                <h4>{genres}</h4>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   </div>
 );
 
