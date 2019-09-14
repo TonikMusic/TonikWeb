10528a10c3ff1019e0cb57e38016078ee1329e8d
KhallilB <khallilbailey@gmail.com>
2019-09-13 20:47:42 -0700

    [add] featured title & styling

--- src/components/landing/featured/Featured.jsx
+++ src/components/landing/featured/Featured.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Featured.css';
 
-const Featured = () => <div>Featured Working!</div>;
+const Featured = () => (
+  <div id="featured" className="landing_featured">
+    <h3 className="landing_featured__title">Featured Artists</h3>
+  </div>
+);
 
 export default Featured;
