1afa352a9524927d27b61ccf8dc56bc9b62db6d2
KhallilB <khallilbailey@gmail.com>
2019-09-13 18:35:27 -0700

    [add] better naming conventions to Hero component

--- src/components/landing/hero/Hero.jsx
+++ src/components/landing/hero/Hero.jsx
@@ -4,13 +4,13 @@ import './Hero.css';
 // First landing view seen
 function Hero() {
   return (
-    <div className="hero">
-      <div className="hero__heading">
+    <div className="landing_hero">
+      <div className="landing_hero__heading">
         <h1>
-          <span id="hero__heading_primary">Build Your Brand</span>
+          <span id="landing_hero__heading_primary">Build Your Brand</span>
         </h1>
         <h3>
-          <span id="hero__heading_secondary">Find opprotunity with Tonik create and expand your community</span>
+          <span id="landing_hero__heading_secondary">Find opprotunity with Tonik create and expand your community</span>
         </h3>
       </div>
     </div>
