2da4dd716b86a526dddee885b85e06027b91d8f6
KhallilB <khallilbailey@gmail.com>
2019-09-15 15:49:10 -0700

    [add] Last Call & Footer components

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -4,12 +4,16 @@ import './Landing.css';
 import Hero from './hero/Hero';
 import About from './about/About';
 import Featured from './featured/Featured';
+import LastCall from './lastCall/LastCall';
+import Footer from './footer/Footer';
 
 const Landing = () => (
   <div>
     <Hero />
     <About />
     <Featured />
+    <LastCall />
+    <Footer />
   </div>
 );
 
---
+++ src/components/landing/lastCall/LastCall.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './LastCall.css';
+
+const LastCall = () => <div>Last Call Works!</div>;
+
+export default LastCall;
