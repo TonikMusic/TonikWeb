21484973de096f3060919ac2842ef04af34d2b18
KhallilB <khallilbailey@gmail.com>
2019-09-16 20:01:38 -0700

    [add] Footer

--- src/components/landing/footer/Footer.jsx
+++ src/components/landing/footer/Footer.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => <div>Footer Works!</div>;
+const Footer = () => (
+  <div className="landing_footer">
+    <div className="landing_footer_text">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </p>
+    </div>
+    <div className="landing_footer_buttons">
+      <div id="landing_signup" type="button">
+        Sign Up
+      </div>
+      <div id="landing_login" type="button">
+        Log In
+      </div>
+    </div>
+  </div>
+);
 
 export default Footer;
