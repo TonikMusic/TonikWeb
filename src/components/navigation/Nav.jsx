735c8065a6a7f22ca3feadc8358cb0b8e186c453
KhallilB <khallilbailey@gmail.com>
2019-09-19 11:03:44 -0700

    [start] Navabar, [change] Folder structure

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
-import LogoColor from '../../assets/MainLogoNoColor.png';
+import LogoColor from '../../assets/Logos/MainLogoNoColor.png';
 
 class Nav extends React.Component {
   constructor() {
@@ -14,31 +14,29 @@ class Nav extends React.Component {
 
   render() {
     return (
-      <div className="nav__container">
-        <nav>
-          <ul className="nav__items_container">
-            <li>
-              <Link to="/">
-                <img src={LogoColor} alt="#" />
-              </Link>
-            </li>
-            <li className="nav__item_artists">Artists</li>
-            <li className="nav__item_explore">Explore</li>
-            <li className="nav__item_about">About</li>
-          </ul>
-          <div className="nav__search">
-            <input type="search" placeholder="Find Artists" />
-          </div>
-          <div className="nav__buttons_container">
-            <button id="nav__buttons_login" type="button">
-              Log In
-            </button>
-            <button id="nav__buttons_signup" type="button">
-              Sign Up
-            </button>
-          </div>
-        </nav>
-      </div>
+      <nav>
+        <ul className="nav__items_container">
+          <li>
+            <Link to="/">
+              <img src={LogoColor} alt="#" />
+            </Link>
+          </li>
+          <li className="nav__item_artists">Artists</li>
+          <li className="nav__item_explore">Explore</li>
+          <li className="nav__item_about">About</li>
+        </ul>
+        <div className="nav__search">
+          <input type="search" placeholder="Find Artists" />
+        </div>
+        <div className="nav__buttons_container">
+          <button id="nav__buttons_login" type="button">
+            Log In
+          </button>
+          <button id="nav__buttons_signup" type="button">
+            Sign Up
+          </button>
+        </div>
+      </nav>
     );
   }
 }
