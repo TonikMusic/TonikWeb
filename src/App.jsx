3e5cae84192090a7fbccacb0897895e893f897ee
KhallilB <khallilbailey@gmail.com>
2019-09-19 18:26:59 -0700

    [change] Router moved from index to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import AuthModal from 'react-modal';
+
+import Nav from './components/navigation/Nav';
 import Landing from './components/landing/Landing';
 
-const App = () => (
-  <div>
-    <Landing />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <div>
+          <AuthModal />
+
+          <Switch>
+            <Route exact path="/" component={Landing} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
