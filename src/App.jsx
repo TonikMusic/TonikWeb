c4351f65434879b0ec7f6499231efd1d988028b6
KhallilB <khallilbailey@gmail.com>
2019-09-23 18:01:33 -0700

    [start] signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React, { Component } from 'react';
 import './App.css';
 
@@ -6,6 +5,7 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Nav from './components/navigation/Nav';
 import Landing from './components/landing/Landing';
+import Signup from './components/auth/signup/Signup';
 
 class App extends Component {
   constructor() {
@@ -21,6 +21,7 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={Landing} />
+            <Route exact path="/signup" component={Signup} />
           </Switch>
         </div>
       </Router>
--- src/components/auth/signup/Signup.jsx
+++ src/components/auth/signup/Signup.jsx
@@ -1,6 +1,28 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './Signup.css';
 
-const Signup = () => <div>Signup Modal</div>;
+class Signup extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="signup__container">
+        <form>
+          <ul className="signup__fields">
+            <li>
+              <label htmlFor="full_name">Full Name</label>
+              <input type="text" id="full_name" placeholder="Enter Your Full Name" />
+            </li>
+          </ul>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Signup;
